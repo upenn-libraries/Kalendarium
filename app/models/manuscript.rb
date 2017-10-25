41255f8334088d56ad69c29d1984aa3bbdf505b6
George Gordon <ggord@RBMGG.LIBRARY.UPENN.EDU>
2017-10-25 14:28:05 -0400

    copy manuscript model code from test_application

--- app/models/manuscript.rb
+++ app/models/manuscript.rb
@@ -1,2 +1,11 @@
 class Manuscript < ApplicationRecord
+  validates_presence_of :name
+
+  NUMBERING_METHODS = %w(foliated paginated)
+
+  LANGUAGES = %w(English French Italian Spanish German Latin Klingon)
+
+  def to_s
+    name
+  end
 end
