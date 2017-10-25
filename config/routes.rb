6d93fceac7e6b8c02d48b4499060c6994f48dd4a
George Gordon <ggord@RBMGG.LIBRARY.UPENN.EDU>
2017-10-25 15:51:44 -0400

    set root to manuscripts index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :manuscripts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'manuscripts#index'
 end
