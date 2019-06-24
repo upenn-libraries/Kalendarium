4d93e2ca9c31c6c2520f0570982b23269bf34120
George Gordon <ggord@upenn.edu>
2019-06-24 16:34:09 -0400

    cleanup comments

--- config/routes.rb
+++ config/routes.rb
@@ -20,12 +20,3 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'manuscripts#index'
 end
-
-
-
-
-
-  #  resources :manuscripts do
-  #   resources :calendar_pages
-  #   resources :feasts
-  # end
